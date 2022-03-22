a4005fb40bc7d7a1994b5945a694833b6f0ff37d
shofiqsaif <sislam161079@gmail.com>
2022-03-22 17:12:40 +0600

    estate module appears as a Module on odoo

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': '$ModuleName',
+    'version': 'Version',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Category',
+    'author': 'Author',
+    'website': 'Website',
+    'license': 'License',
+    'depends': ['Depends'],
+    'data': ['Data'],
+    'demo': ['Demo'],
+    'installable': True,
+    'auto_install': False
+}
