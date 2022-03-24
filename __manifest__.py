be694450fd3eb89d71972f868941428f60350110
shofiqul-bs23 <shofiqul.islam@brainstation-23.com>
2022-03-24 16:30:23 +0600

    Added estate.property.type model and related views. Added 3 Many2one field to estate.property model and updated the views accordingly.

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,10 @@
         'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+
         'views/estate_menus.xml'
+
 
     ]
 }
--- models/estate_property.py
+++ models/estate_property.py
@@ -2,6 +2,8 @@ import odoo.odoo.fields
 from odoo import fields, models
 #from odoo.odoo import fields
 from dateutil.relativedelta import relativedelta
+def getThreeMonthLaterDate():
+    return fields.Date.today() + relativedelta(months=3)
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -11,7 +13,8 @@ class EstateProperty(models.Model):
     description = fields.Text()
     postcode = fields.Char()
     # date_availability = fields.Date(copy=False,default= fields.Date(default=lambda record: fields.Date.today() + relativedelta(days=30)))
-    date_availability = fields.Date(default=lambda record: fields.Date.today() + relativedelta(months=3))
+    # date_availability = fields.Date(default=lambda record: fields.Date.today() + relativedelta(months=3))
+    date_availability = fields.Date(default = getThreeMonthLaterDate())
     expected_price = fields.Float(required = True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
@@ -31,4 +34,9 @@ class EstateProperty(models.Model):
             ('sold','Sold'),('canceled','Canceled')
         ]
     )
+
+    property_type_id = fields.Many2one('estate.property.type')
+    # salesperson_id = fields.Many2one('')
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', index=True, tracking=True)
 
