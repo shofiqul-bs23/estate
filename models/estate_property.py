f3c8bb3d8d0000a4b149df4d0f057ea42c08f506
shofiqul-bs23 <shofiqul.islam@brainstation-23.com>
2022-03-27 14:06:32 +0600

    Best offer has been calculated

--- models/estate_property.py
+++ models/estate_property.py
@@ -47,14 +47,18 @@ class EstateProperty(models.Model):
     offer_ids = fields.One2many('estate.property.offer','property_id',string='Offers')
 
     total_area = fields.Float(compute = '_compute_total_area')
-    # best_price = fields.Float(compute = '_compute_highest_offer')
+
+    best_offer = fields.Float(compute = '_compute_best_offer')
 
 
-    # @api.depends('living_area', 'garden_area')
-    # def _compute_total_area(self):
-    #     for record in self:
-    #         record.total_area = record.living_area + record.garden_area
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
 
-
+    @api.depends('offer_ids')
+    def _compute_best_offer(self):
+        for record in self:
+            record.best_offer = max(record.offer_ids.mapped('price'))
 
 
